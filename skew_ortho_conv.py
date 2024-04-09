5e13bfbc38b6ba35847e1c71d014493d6ab40d80
Kilka74 <mikolka1337@gmail.com>
2024-04-09 16:47:04 +0300

    grouped skew orthogonal initial version

--- skew_ortho_conv.py
+++ skew_ortho_conv.py
@@ -6,45 +6,58 @@ import numpy as np
 import einops
 
 
-def fantastic_four(conv_filter, num_iters=50):
+# TODO change fantastic four, add groups number for each tensor
+def fantastic_four(conv_filter, groups, num_iters=50):
     out_ch, in_ch, h, w = conv_filter.shape
 
-    u1 = torch.randn((1, in_ch, 1, w), device='cuda', requires_grad=False)
+    u1 = torch.randn((groups, 1, in_ch, 1, w), device="cuda", requires_grad=False)
     u1.data = l2_normalize(u1.data)
 
-    u2 = torch.randn((1, in_ch, h, 1), device='cuda', requires_grad=False)
+    u2 = torch.randn((groups, 1, in_ch, h, 1), device="cuda", requires_grad=False)
     u2.data = l2_normalize(u2.data)
 
-    u3 = torch.randn((1, in_ch, h, w), device='cuda', requires_grad=False)
+    u3 = torch.randn((groups, 1, in_ch, h, w), device="cuda", requires_grad=False)
     u3.data = l2_normalize(u3.data)
 
-    u4 = torch.randn((out_ch, 1, h, w), device='cuda', requires_grad=False)
+    u4 = torch.randn((groups, out_ch, 1, h, w), device="cuda", requires_grad=False)
     u4.data = l2_normalize(u4.data)
 
-    v1 = torch.randn((out_ch, 1, h, 1), device='cuda', requires_grad=False)
+    v1 = torch.randn((groups, out_ch, 1, h, 1), device="cuda", requires_grad=False)
     v1.data = l2_normalize(v1.data)
 
-    v2 = torch.randn((out_ch, 1, 1, w), device='cuda', requires_grad=False)
+    v2 = torch.randn((groups, out_ch, 1, 1, w), device="cuda", requires_grad=False)
     v2.data = l2_normalize(v2.data)
 
-    v3 = torch.randn((out_ch, 1, 1, 1), device='cuda', requires_grad=False)
+    v3 = torch.randn((groups, out_ch, 1, 1, 1), device="cuda", requires_grad=False)
     v3.data = l2_normalize(v3.data)
 
-    v4 = torch.randn((1, in_ch, 1, 1), device='cuda', requires_grad=False)
+    v4 = torch.randn((groups, 1, in_ch, 1, 1), device="cuda", requires_grad=False)
     v4.data = l2_normalize(v4.data)
 
     for i in range(num_iters):
-        v1.data = l2_normalize((conv_filter.data*u1.data).sum((1, 3), keepdim=True).data)
-        u1.data = l2_normalize((conv_filter.data*v1.data).sum((0, 2), keepdim=True).data)
+        v1.data = l2_normalize(
+            (conv_filter.data * u1.data).sum((2, 4), keepdim=True).data
+        )
+        u1.data = l2_normalize(
+            (conv_filter.data * v1.data).sum((1, 3), keepdim=True).data
+        )
 
-        v2.data = l2_normalize((conv_filter.data*u2.data).sum((1, 2), keepdim=True).data)
-        u2.data = l2_normalize((conv_filter.data*v2.data).sum((0, 3), keepdim=True).data)
+        v2.data = l2_normalize(
+            (conv_filter.data * u2.data).sum((2, 3), keepdim=True).data
+        )
+        u2.data = l2_normalize(
+            (conv_filter.data * v2.data).sum((1, 4), keepdim=True).data
+        )
 
-        v3.data = l2_normalize((conv_filter.data*u3.data).sum((1, 2, 3), keepdim=True).data)
-        u3.data = l2_normalize((conv_filter.data*v3.data).sum(0, keepdim=True).data)
+        v3.data = l2_normalize(
+            (conv_filter.data * u3.data).sum((2, 3, 4), keepdim=True).data
+        )
+        u3.data = l2_normalize((conv_filter.data * v3.data).sum(1, keepdim=True).data)
 
-        v4.data = l2_normalize((conv_filter.data*u4.data).sum((0, 2, 3), keepdim=True).data)
-        u4.data = l2_normalize((conv_filter.data*v4.data).sum(1, keepdim=True).data)
+        v4.data = l2_normalize(
+            (conv_filter.data * u4.data).sum((1, 3, 4), keepdim=True).data
+        )
+        u4.data = l2_normalize((conv_filter.data * v4.data).sum(2, keepdim=True).data)
 
     return u1, v1, u2, v2, u3, v3, u4, v4
 
@@ -57,8 +70,8 @@ def l2_normalize(tensor, eps=1e-12):
 
 
 def transpose_filter(conv_filter):
-    conv_filter_T = torch.transpose(conv_filter, 0, 1)    
-    conv_filter_T = torch.flip(conv_filter_T, [2, 3])
+    conv_filter_T = torch.transpose(conv_filter, 1, 2)
+    conv_filter_T = torch.flip(conv_filter_T, [3, 4])
     return conv_filter_T
 
 
@@ -66,42 +79,57 @@ class SOC_Function(Function):
     @staticmethod
     def forward(ctx, curr_z, conv_filter):
         ctx.conv_filter = conv_filter
+        n_groups = conv_filter.shape[0] // conv_filter.shape[1]
+        ctx.n_groups = n_groups
         kernel_size = conv_filter.shape[2]
         z = curr_z
-        curr_fact = 1.
         for i in range(1, 14):
-            curr_z = F.conv2d(curr_z, conv_filter,
-                              padding=(kernel_size//2,
-                                       kernel_size//2))/float(i)
+            curr_z = F.conv2d(
+                curr_z, conv_filter, padding=(kernel_size // 2, kernel_size // 2), groups=n_groups
+            ) / float(i)
             z = z + curr_z
         return z
 
     @staticmethod
     def backward(ctx, grad_output):
         conv_filter = ctx.conv_filter
+        n_groups = ctx.n_groups
         kernel_size = conv_filter.shape[2]
         grad_input = grad_output
-        curr_fact = 1.
         for i in range(1, 14):
-            grad_output = F.conv2d(grad_output, -conv_filter,
-                              padding=(kernel_size//2,
-                                       kernel_size//2))/float(i)
+            grad_output = F.conv2d(
+                grad_output, -conv_filter, padding=(kernel_size // 2, kernel_size // 2), groups=n_groups
+            ) / float(i)
             grad_input = grad_input + grad_output
 
         return grad_input, None
 
 
 class SOC(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=None,
-                 bias=True, train_terms=5, eval_terms=12, init_iters=50, update_iters=1,
-                 update_freq=200, correction=0.7):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        stride=1,
+        padding=None,
+        bias=True,
+        groups=1,
+        train_terms=5,
+        eval_terms=12,
+        init_iters=50,
+        update_iters=1,
+        update_freq=200,
+        correction=0.7,
+    ):
         super(SOC, self).__init__()
-        assert (stride==1) or (stride==2)
+        assert (stride == 1) or (stride == 2)
         self.init_iters = init_iters
         self.out_channels = out_channels
-        self.in_channels = in_channels*stride*stride
-        self.max_channels = max(self.out_channels, self.in_channels)
-
+        self.in_channels = in_channels * stride * stride
+        self.groups = groups
+        assert max(self.out_channels, self.in_channels) % self.groups == 0
+        self.max_channels = max(self.out_channels, self.in_channels) // self.groups
 
         self.stride = stride
         self.kernel_size = kernel_size
@@ -114,16 +142,25 @@ class SOC(nn.Module):
         if kernel_size == 1:
             correction = 1.0
 
-        self.random_conv_filter = nn.Parameter(torch.Tensor(torch.randn(self.max_channels, 
-                                               self.max_channels, self.kernel_size, 
-                                               self.kernel_size)).cuda(),
-                                               requires_grad=True)
+        self.random_conv_filter = nn.Parameter(
+            torch.Tensor(
+                torch.randn(
+                    self.groups,
+                    self.max_channels,
+                    self.max_channels,
+                    self.kernel_size,
+                    self.kernel_size,
+                )
+            ).cuda(),
+            requires_grad=True,
+        )
         random_conv_filter_T = transpose_filter(self.random_conv_filter)
-        conv_filter = 0.5*(self.random_conv_filter - random_conv_filter_T)
+        conv_filter = 0.5 * (self.random_conv_filter - random_conv_filter_T)
 
         with torch.no_grad():
-            u1, v1, u2, v2, u3, v3, u4, v4 = fantastic_four(conv_filter,
-                                                num_iters=self.init_iters)
+            u1, v1, u2, v2, u3, v3, u4, v4 = fantastic_four(
+                conv_filter, num_iters=self.init_iters, groups=self.groups
+            )
             self.u1 = nn.Parameter(u1, requires_grad=False)
             self.v1 = nn.Parameter(v1, requires_grad=False)
             self.u2 = nn.Parameter(u2, requires_grad=False)
@@ -133,12 +170,15 @@ class SOC(nn.Module):
             self.u4 = nn.Parameter(u4, requires_grad=False)
             self.v4 = nn.Parameter(v4, requires_grad=False)
 
-        self.correction = nn.Parameter(torch.Tensor([correction]).cuda(), requires_grad=False)
+        self.correction = nn.Parameter(
+            torch.Tensor([correction]).cuda(), requires_grad=False
+        )
 
         self.enable_bias = bias
         if self.enable_bias:
             self.bias = nn.Parameter(
-                torch.Tensor(self.out_channels).cuda(), requires_grad=True)
+                torch.Tensor(self.out_channels).cuda(), requires_grad=True
+            )
         else:
             self.bias = None
         self.reset_parameters()
@@ -151,6 +191,7 @@ class SOC(nn.Module):
         if self.bias is not None:
             nn.init.uniform_(self.bias, -stdv, stdv)
 
+    # TODO change update_sigma for grouping
     def update_sigma(self):
         if self.training:
             if self.total_iters % self.update_freq == 0:
@@ -162,40 +203,53 @@ class SOC(nn.Module):
             update_iters = 0
 
         random_conv_filter_T = transpose_filter(self.random_conv_filter)
-        conv_filter = 0.5*(self.random_conv_filter - random_conv_filter_T)
-        pad_size = conv_filter.shape[2]//2
+        conv_filter = 0.5 * (self.random_conv_filter - random_conv_filter_T)
+        # pad_size = conv_filter.shape[2] // 2
         with torch.no_grad():
             for i in range(update_iters):
-                self.v1.data = l2_normalize((conv_filter*self.u1).sum(
-                                            (1, 3), keepdim=True).data)
-                self.u1.data = l2_normalize((conv_filter*self.v1).sum(
-                                            (0, 2), keepdim=True).data)
-                self.v2.data = l2_normalize((conv_filter*self.u2).sum(
-                                            (1, 2), keepdim=True).data)
-                self.u2.data = l2_normalize((conv_filter*self.v2).sum(
-                                            (0, 3), keepdim=True).data)
-                self.v3.data = l2_normalize((conv_filter*self.u3).sum(
-                                            (1, 2, 3), keepdim=True).data)
-                self.u3.data = l2_normalize((conv_filter*self.v3).sum(
-                                            0, keepdim=True).data)
-                self.v4.data = l2_normalize((conv_filter*self.u4).sum(
-                                            (0, 2, 3), keepdim=True).data)
-                self.u4.data = l2_normalize((conv_filter*self.v4).sum(
-                                            1, keepdim=True).data)
+                self.v1.data = l2_normalize(
+                    (conv_filter * self.u1).sum((2, 4), keepdim=True).data
+                )
+                self.u1.data = l2_normalize(
+                    (conv_filter * self.v1).sum((1, 3), keepdim=True).data
+                )
+                self.v2.data = l2_normalize(
+                    (conv_filter * self.u2).sum((2, 3), keepdim=True).data
+                )
+                self.u2.data = l2_normalize(
+                    (conv_filter * self.v2).sum((1, 4), keepdim=True).data
+                )
+                self.v3.data = l2_normalize(
+                    (conv_filter * self.u3).sum((2, 3, 4), keepdim=True).data
+                )
+                self.u3.data = l2_normalize(
+                    (conv_filter * self.v3).sum(1, keepdim=True).data
+                )
+                self.v4.data = l2_normalize(
+                    (conv_filter * self.u4).sum((1, 3, 4), keepdim=True).data
+                )
+                self.u4.data = l2_normalize(
+                    (conv_filter * self.v4).sum(2, keepdim=True).data
+                )
 
         func = torch.min
-        sigma1 = torch.sum(conv_filter*self.u1*self.v1)
-        sigma2 = torch.sum(conv_filter*self.u2*self.v2)
-        sigma3 = torch.sum(conv_filter*self.u3*self.v3)
-        sigma4 = torch.sum(conv_filter*self.u4*self.v4)
+        sigma1 = torch.sum(conv_filter * self.u1 * self.v1)
+        sigma2 = torch.sum(conv_filter * self.u2 * self.v2)
+        sigma3 = torch.sum(conv_filter * self.u3 * self.v3)
+        sigma4 = torch.sum(conv_filter * self.u4 * self.v4)
         sigma = func(func(func(sigma1, sigma2), sigma3), sigma4)
         return sigma
 
     def forward(self, x):
         random_conv_filter_T = transpose_filter(self.random_conv_filter)
-        conv_filter_skew = 0.5*(self.random_conv_filter - random_conv_filter_T)
+        conv_filter_skew = 0.5 * (self.random_conv_filter - random_conv_filter_T)
         sigma = self.update_sigma()
-        conv_filter_n = (self.correction * conv_filter_skew)/sigma
+        conv_filter_n = ((self.correction * conv_filter_skew) / sigma).view(
+            self.groups * self.max_channels,
+            self.max_channels,
+            self.kernel_size,
+            self.kernel_size,
+        )
 
         if self.training:
             num_terms = self.train_terms
@@ -203,8 +257,12 @@ class SOC(nn.Module):
             num_terms = self.eval_terms
 
         if self.stride > 1:
-            x = einops.rearrange(x, "b c (w k1) (h k2) -> b (c k1 k2) w h",
-                                 k1=self.stride, k2=self.stride)
+            x = einops.rearrange(
+                x,
+                "b c (w k1) (h k2) -> b (c k1 k2) w h",
+                k1=self.stride,
+                k2=self.stride,
+            )
 
         if self.out_channels > self.in_channels:
             diff_channels = self.out_channels - self.in_channels
@@ -214,14 +272,17 @@ class SOC(nn.Module):
             curr_z = x
 
         z = curr_z
-        for i in range(1, num_terms+1):
-            curr_z = F.conv2d(curr_z, conv_filter_n,
-                              padding=(self.kernel_size//2,
-                                       self.kernel_size//2))/float(i)
+        for i in range(1, num_terms + 1):
+            curr_z = F.conv2d(
+                curr_z,
+                conv_filter_n,
+                padding=(self.kernel_size // 2, self.kernel_size // 2),
+                groups=self.groups,
+            ) / float(i)
             z = z + curr_z
 
         if self.out_channels < self.in_channels:
-            z = z[:, :self.out_channels, :, :]
+            z = z[:, : self.out_channels, :, :]
 
         if self.enable_bias:
             z = z + self.bias.view(1, -1, 1, 1)
