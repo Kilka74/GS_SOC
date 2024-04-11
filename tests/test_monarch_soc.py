55dc4fb820c355678f2282f8c6055851d5e57ce4
Kilka74 <mikolka1337@gmail.com>
2024-04-11 20:45:20 +0300

    Monarch SOC implementation Added grouping to SOC Implemented and tested MonarchSOC Add files from original soc repo

--- tests/test_monarch_soc.py
+++ tests/test_monarch_soc.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+import torch.nn.functional as F
 from skew_ortho_conv import MonarchSOC, channel_shuffle
 
 
@@ -8,11 +9,12 @@ from skew_ortho_conv import MonarchSOC, channel_shuffle
     ["device", "groups"],
     [
         ["cpu", 1],
-        # ["cpu", 4]
+        ["cpu", 4]
     ]
 )
 def test_monarch_soc(device, groups):
     x = torch.randn(4, 64, 64, 64)
+    x /= torch.norm(x)
     soc = MonarchSOC(
         in_channels=64,
         out_channels=64,
@@ -20,8 +22,30 @@ def test_monarch_soc(device, groups):
         padding=1,
         groups=groups,
         device=device,
-        testing=True
+        testing=True,
+        bias=False
     )
-    y = channel_shuffle(soc(x), groups=groups)
-    assert y.shape == (4, 64, 64, 64), y.shape
-    assert torch.allclose(y, x), torch.norm(y - x)
+    res, filter_1, filter_2 = soc(x)
+    assert not torch.any(res.isnan())
+    curr_x = x
+    for i in range(1, 21):
+        curr_x = F.conv2d(
+            curr_x,
+            filter_1,
+            padding=(1, 1),
+            groups=groups
+        ) / float(i)
+        x = x + curr_x
+    assert not torch.any(x.isnan())
+    x = channel_shuffle(x, groups=groups)
+    curr_x = x
+    for i in range(1, 21):
+        curr_x = F.conv2d(
+            curr_x,
+            filter_2,
+            padding=(1, 1),
+            groups=groups
+        ) / float(i)
+        x = x + curr_x
+    assert res.shape == x.shape
+    assert torch.allclose(x, res), torch.norm(res - x)
