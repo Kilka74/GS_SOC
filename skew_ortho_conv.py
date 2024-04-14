fix l2 normalization

skew_ortho_conv.py
@@ -61,8 +61,9 @@ def fantastic_four(conv_filter, num_iters=50, device="cuda"):
     return u1, v1, u2, v2, u3, v3, u4, v4
 
 
+# Probably bad normalization because of norm computation
 def l2_normalize(tensor, eps=1e-12):
-    norm = torch.sqrt(torch.sum(tensor.float() * tensor.float(), dim=0))
+    norm = torch.sqrt(torch.sum(tensor.float() * tensor.float(), dim=(1, 2), keepdim=True))
     norm = torch.max(norm, torch.tensor([eps], device=norm.device))
     ans = tensor / norm
     return ans

train_standard.py
@@ -87,7 +87,6 @@ def main(args):
             "max_lr": args.lr_max,
             "weight_decay": args.weight_decay,
             "momentum": args.momentum,
-            "opt_level": args.opt_level,
             "number_of_parameters": sum(p.numel() for p in model.parameters())
         }
     )
@@ -138,7 +137,7 @@ def main(args):
         )
     elif args.lr_scheduler == "multistep":
         scheduler = torch.optim.lr_scheduler.MultiStepLR(
-            opt, milestones=[lr_steps // 2, (3 * lr_steps) // 4], gamma=0.1
+            opt, milestones=[lr_steps // 4, (3 * lr_steps) // 4], gamma=0.1
         )
 
     best_model_path = os.path.join(args.out_dir, "best.pth")

utils.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 import math
 from skew_ortho_conv import SOC, MonarchSOC
+from original_soc import SOC as OriginalSOC
 from custom_activations import MaxMin, HouseHolder, HouseHolder_Order_2
 
 cifar10_mean = (0.4914, 0.4822, 0.4465)
@@ -285,7 +286,7 @@ def evaluate_certificates(test_loader, model, L, epsilon=36.0):
 # from cayley_ortho_conv import Cayley, CayleyLinear
 # from block_ortho_conv import BCOP
 
-conv_mapping = {"standard": nn.Conv2d, "soc": SOC, "monarch_soc": MonarchSOC}
+conv_mapping = {"standard": nn.Conv2d, "soc": SOC, "monarch_soc": MonarchSOC, "original_soc": OriginalSOC}
 
 
 activation_dict = {