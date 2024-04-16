fantastic four fix Try to fix fantastic four implementation in case of grouped convolution. Make normalization for every convlution group separately

skew_ortho_conv.py
@@ -63,7 +63,9 @@ def fantastic_four(conv_filter, num_iters=50, device="cuda"):
 
 # Probably bad normalization because of norm computation
 def l2_normalize(tensor, eps=1e-12):
-    norm = torch.sqrt(torch.sum(tensor.float() * tensor.float(), dim=(1, 2), keepdim=True))
+    ndims = torch.ndim(tensor)
+    dims = tuple(torch.arange(1, ndims))
+    norm = torch.sqrt(torch.sum(tensor.float() * tensor.float(), dim=dims, keepdim=True))
     norm = torch.max(norm, torch.tensor([eps], device=norm.device))
     ans = tensor / norm
     return ans
@@ -236,10 +238,11 @@ class SOC(nn.Module):
         func = torch.min
         # add sum by dimension because we deal with 5-dimensional tensor and we want
         # to compute approximation for each group separately
-        sigma1 = torch.sum(conv_filter * self.u1 * self.v1)
-        sigma2 = torch.sum(conv_filter * self.u2 * self.v2)
-        sigma3 = torch.sum(conv_filter * self.u3 * self.v3)
-        sigma4 = torch.sum(conv_filter * self.u4 * self.v4)
+        dims = (1, 2, 3, 4)
+        sigma1 = torch.sum(conv_filter * self.u1 * self.v1, dim=dims, keepdim=True)
+        sigma2 = torch.sum(conv_filter * self.u2 * self.v2, dim=dims, keepdim=True)
+        sigma3 = torch.sum(conv_filter * self.u3 * self.v3, dim=dims, keepdim=True)
+        sigma4 = torch.sum(conv_filter * self.u4 * self.v4, dim=dims, keepdim=True)
         sigma = func(func(func(sigma1, sigma2), sigma3), sigma4)
         return sigma
 

train_standard.py
@@ -149,7 +149,6 @@ def main(args):
     logger.info(
         "Epoch \t Seconds \t LR \t Train Loss \t Train Acc \t Test Loss \t Test Acc"
     )
-    scaler = torch.cuda.amp.GradScaler()
     for epoch in range(args.epochs):
         model.train()
         start_epoch_time = time.time()
@@ -157,19 +156,18 @@ def main(args):
         train_acc = 0
         train_n = 0
         for i, (X, y) in enumerate(train_loader):
-            with torch.autocast(device_type="cuda", dtype=torch.float16):
-                X, y = X.cuda(), y.cuda()
+            # with torch.autocast(device_type="cuda", dtype=torch.float16):
+            X, y = X.cuda(), y.cuda()
 
-                output = model(X)
-                ce_loss = criterion(output, y)
-                wandb.log({
-                    "train_loss": ce_loss.item(),
-                    "lr": scheduler.get_last_lr()[0]
-                })
+            output = model(X)
+            ce_loss = criterion(output, y)
+            wandb.log({
+                "train_loss": ce_loss.item(),
+                "lr": scheduler.get_last_lr()[0]
+            })
             opt.zero_grad(set_to_none=True)
-            scaler.scale(ce_loss).backward()
-            scaler.step(opt)
-            scaler.update()
+            ce_loss.backward()
+            opt.step()
 
             train_loss += ce_loss.item() * y.size(0)
             train_acc += (output.max(1)[1] == y).sum().item()

utils.py
@@ -98,7 +98,6 @@ def attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts, opt=None):
                 break
             loss = F.cross_entropy(output, y)
             if opt is not None:
-                # TODO try to use this implementation or to replace it with GradScaler
                 loss.backward()
             else:
                 loss.backward()
@@ -158,7 +157,6 @@ def attack_pgd_l2(model, X, y, epsilon, alpha, attack_iters, restarts, opt=None)
                 break
             loss = F.cross_entropy(output, y)
             if opt is not None:
-                # TODO try to use this implementation or to replace it with GradScaler
                 loss.backward()
             else:
                 loss.backward()
@@ -207,13 +205,13 @@ def evaluate_standard(test_loader, model):
     model.eval()
     with torch.no_grad():
         for i, (X, y) in enumerate(test_loader):
-            with torch.autocast(device_type="cuda", dtype=torch.float16):
-                X, y = X.cuda(), y.cuda()
-                output = model(X)
-                loss = F.cross_entropy(output, y)
-                test_loss += loss.item() * y.size(0)
-                test_acc += (output.max(1)[1] == y).sum().item()
-                n += y.size(0)
+            # with torch.autocast(device_type="cuda", dtype=torch.float16):
+            X, y = X.cuda(), y.cuda()
+            output = model(X)
+            loss = F.cross_entropy(output, y)
+            test_loss += loss.item() * y.size(0)
+            test_acc += (output.max(1)[1] == y).sum().item()
+            n += y.size(0)
     return test_loss / n, test_acc / n
 
 